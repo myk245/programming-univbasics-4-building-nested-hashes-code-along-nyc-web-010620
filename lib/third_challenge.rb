87cfb91e52f05115c17dbe4b50ae84a407cf84b9
Cindy Kei <myk245@nyu.edu>
2019-11-17 00:21:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,13 +16,13 @@ def third_challenge
         },
       :hero_friends => [
         {
-          :name => "Benvolio", 
-          :age => "17", 
+          :name => "Benvolio",
+          :age => "17",
           :attitude => "worried"
-        }, 
+        },
         {
-          :name => "Mercutio", 
-          :age => "18", 
+          :name => "Mercutio",
+          :age => "18",
           :attitude => "hot-headed"
         }
       ]
